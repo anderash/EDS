d1aa051202857a7d52109451b8c59e11efa12c2a
anderash <anders.aasheim@hotmail.com>
2015-03-18 11:24:59 +0100

    La til driver setup

--- ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -1,10 +1,9 @@
-/*
- * This is a demo Linux kernel module.
- */
 
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+
+#include "efm32gg.h"
 
 /*
  * template_init - function to insert this module into kernel space
@@ -17,7 +16,19 @@
 
 static int __init template_init(void)
 {
-	printk("Hello World, here is your module speaking\n");
+	printk("Hello World, gamepad driver is setting up\n");
+
+	*GPIO_PA_CTRL 		= 2;  				// set high drive strength
+	*GPIO_PA_MODEH		= 0x55555555; // set pins A8-15 as output
+	*GPIO_PA_DOUT 		= 0xff00; 		// turn off all LEDs(LEDs are active low)
+
+	*GPIO_PC_MODEL 		= 0x33333333; // Set pins 0-7 to input
+	*GPIO_PC_DOUT 		= 0xFF; 			// Enable internal pull-up
+
+	*GPIO_EXTIPSELL 	= 0x22222222; // 
+	*GPIO_EXTIFALL		= 0xFF;				//Set interrupt on neg-edge
+	//*GPIO_EXTIRISE		= 0xFF;				// Set interrupt on pos-edge
+	*GPIO_IEN		= 0xFF;				// Enable interrupt generation
 	return 0;
 }
 
