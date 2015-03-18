1c0adaf179ca056952f9a73c70ef710296380b54
anderash <anders.aasheim@hotmail.com>
2015-03-18 14:24:48 +0100

    First step

--- ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <linux/fb.h>
+
+struct fb_copyarea rect;
 
 int main(int argc, char *argv[])
 {
 	printf("Hello World, I'm game!\n");
+	printf("The game is on... AndyA7 won! Havlov91 and FreddyBoy lost\n");
+
+
 
 	exit(EXIT_SUCCESS);
 }
