2f3c4952f69c1714ef8315431164f8b1038d0ca6
havlov91 <tdt4258@tdt4258-VirtualBox.(none)>
2015-04-24 11:33:30 +0200

    driver med kommentarer

--- ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -1,7 +1,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/fs.h> //fasync
+#include <linux/fs.h> 
 #include <linux/proc_fs.h>
 #include <linux/ioport.h>
 #include <linux/cdev.h>
@@ -10,7 +10,7 @@
 #include <linux/kdev_t.h>
 #include <linux/ioport.h>
 #include <asm/io.h>
-#include <linux/unistd.h> //read
+#include <linux/unistd.h>
 #include <asm/uaccess.h>
 #include <asm/signal.h>
 #include <asm/siginfo.h>
@@ -68,43 +68,38 @@ static int __init my_init(void)
 	/* Dynamically allocate device numbers */
 	result = alloc_chrdev_region(&device_nr, 0, DEV_NR_COUNT, DRIVER_NAME);
 	if (result < 0) {
-		printk(KERN_ALERT "Failed to allocate device numbers\n");
+		printk(KERN_ALERT "Failed to allocate major numbers\n");
 		return -1;
 	}
 
 	if (request_mem_region(GPIO_PC_MODEL, 1, DRIVER_NAME) == NULL ) {
-		printk(KERN_ALERT "Error requesting GPIO_PC_MODEL memory region, already in use?\n");
+		printk(KERN_ALERT "Error requesting GPIO_PC_MODEL memory region\n");
 		return -1;
 	}
 	if (request_mem_region(GPIO_PC_DOUT, 1, DRIVER_NAME) == NULL ) {
-		printk(KERN_ALERT "Error requesting GPIO_PC_DOUT memory region, already in use?\n");
+		printk(KERN_ALERT "Error requesting GPIO_PC_DOUT memory region\n");
 		return -1;
 	}
 	if (request_mem_region(GPIO_PC_DIN, 1, DRIVER_NAME) == NULL ) {
-		printk(KERN_ALERT "Error requesting GPIO_PC_DIN memory region, already in use?\n");
+		printk(KERN_ALERT "Error requesting GPIO_PC_DIN memory region\n");
 		return -1;
 	}
-	//ioremap_nocache(GPIO_PA_BASE, GPIO_IFC - GPIO_PA_BASE);
-	/* Init gpio as in previous exercises.
-	* For portability, these writes should be performed with a base address
-	* obtained from the ioremap_nocache call above and an offset. What we are
-	* doing below is possible since we're not using virtual memory.
-	*/
+
 	iowrite32(0x33333333, GPIO_PC_MODEL);
 	iowrite32(0xFF, GPIO_PC_DOUT);
 	iowrite32(0x22222222, GPIO_EXTIPSELL);
-	/* Setup for interrupts */
+	// Request IRQ-lines (odd and even)
 	 request_irq(GPIO_EVEN_IRQ_LINE, (irq_handler_t)gpio_interrupt_handler, 0, DRIVER_NAME, &my_cdev);
 	request_irq(GPIO_ODD_IRQ_LINE, (irq_handler_t)gpio_interrupt_handler, 0, DRIVER_NAME, &my_cdev);
 
-	/* add device */
+	// registrer device 
 	cdev_init(&my_cdev, &my_fops);
 	my_cdev.owner = THIS_MODULE;
 	cdev_add(&my_cdev, device_nr, DEV_NR_COUNT);
 	cl = class_create(THIS_MODULE, DRIVER_NAME);
 	device_create(cl, NULL, device_nr, NULL, DRIVER_NAME);
 
-	/* Actually enable interrupts */
+	// enabling interrupts
 	iowrite32(0xFF, GPIO_EXTIFALL);
 	iowrite32(0x00FF, GPIO_IEN);
 	iowrite32(0xFF, GPIO_IFC);
@@ -118,21 +113,20 @@ static void __exit my_exit(void)
 {
 	printk("Unloading gamepad driver\n");
 
-	//remove_proc_entry("fasync", NULL);
-	/* Disable interrupts */
+	// disable interrupts
 	iowrite32(0x0000, GPIO_IEN);
-	/* Free irq */
+	// release irq-lines
 	free_irq(GPIO_EVEN_IRQ_LINE, &my_cdev);
 	free_irq(GPIO_ODD_IRQ_LINE, &my_cdev);
-	/* Release memory regions */
+	// release memory regions
 	release_mem_region(GPIO_PC_MODEL, 1);
 	release_mem_region(GPIO_PC_DIN, 1);
 	release_mem_region(GPIO_PC_DOUT, 1);
-	/* Remove device */
+	// "delete" device
 	device_destroy(cl, device_nr);
 	class_destroy(cl);
 	cdev_del(&my_cdev);
-	/* Dealloc the device numbers */
+	// free device number
 	unregister_chrdev_region(device_nr, DEV_NR_COUNT);
 }
 
@@ -148,14 +142,14 @@ static int my_release(struct inode *inode, struct file *filp)
 }
 static ssize_t my_read(struct file *filp, char __user *buff, size_t count, loff_t *offp)
 {
-	 /* Read gpio button status and write to buff */
+	// read data from buttons 
 	uint32_t data = ioread32(GPIO_PC_DIN);
 	copy_to_user(buff, &data, 1);
 	return 1;
 }
 static ssize_t my_write(struct file *filp, const char __user *buff, size_t count, loff_t *offp)
 {
-	printk(KERN_INFO "Writing to buttons doesn't make sense.");
+	printk(KERN_INFO "Writing to device..");
 	return 1;
 }
 
